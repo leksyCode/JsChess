314e07e886034280553edd0cd9fd14d4f81185ee
Aleksey Nikolaev <nikolaev.profent@gmail.com>
2020-05-16 16:21:01 +0300

    add rook steps

--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -38,6 +38,9 @@ function makeStep(ev) {
     if (data.charAt(1) == 'P') {
         pawnStep(ev, targetId, currentId);
     }
+    else if (data.charAt(1) == 'R') {
+        rookStep(ev, targetId, currentId);
+    }
     else {
         ev.target.appendChild(document.getElementById(data));
     }
@@ -76,3 +79,71 @@ function pawnStep(ev, targetId, currentId) {
     }   
 }
 
+function rookStep(ev, targetId, currentId) {
+    var data = ev.dataTransfer.getData("text");
+
+    if (targetId.charAt(0) == currentId.charAt(0) || targetId.charAt(1) == currentId.charAt(1)) { // if targer in the same vertical and gorizontal coordinate
+        // Checks if there is another figure in the path
+        if (checkVert(currentId.charAt(0), Number(currentId.charAt(1)), targetId.charAt(0), Number(targetId.charAt(1)))) {
+            ev.target.appendChild(document.getElementById(data));
+        }
+    }      
+}
+
+function checkVert(startX, startY, destX, destY) {     
+    if (startY < destY) { // checking up
+        while (startY != destY) {
+            if (document.getElementById(startX + (startY + 1)).firstElementChild.firstElementChild!= null) {
+                return false;
+            }
+            else {
+                startY++;
+            }
+            if (startY == destY) {
+                return true;
+            }
+        }  
+    } 
+    else if (startY > destY) { // checking down
+        while (startY != destY) {
+            if (document.getElementById(startX + (startY - 1)).firstElementChild.firstElementChild != null) {
+                return false;
+            }
+            else {
+                startY--;
+            }
+            if (startY == destY) {
+                return true;
+            }
+        }  
+    }
+    else if (startX.charCodeAt(0) < destX.charCodeAt(0)) { // checking right
+        while (startX != destX) {
+            var nextVal = String.fromCharCode(startX.charCodeAt(0) + 1); // set next ASCII value
+            if (document.getElementById(nextVal + startY).firstElementChild.firstElementChild != null) {
+                return false;
+            }
+            else {
+                startX = nextVal; 
+            }
+            if (startX == destX) {
+                return true;
+            }
+        }  
+    }
+    else if (startX.charCodeAt(0) > destX.charCodeAt(0)) { // checking left
+        while (startX != destX) {
+            var nextVal = String.fromCharCode(startX.charCodeAt(0) - 1); // set next ASCII value
+            if (document.getElementById(nextVal + startY).firstElementChild.firstElementChild != null) {
+                return false;
+            }
+            else {
+                startX = nextVal; 
+            }
+            if (startX == destX) {
+                return true;
+            }
+        }
+    }
+   
+}
