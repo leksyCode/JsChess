8c715bbf1594cd3c91ffa48730c64d4a41a14c01
Aleksey Nikolaev <nikolaev.profent@gmail.com>
2020-05-16 15:10:02 +0300

    add pawn steps

--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -1,32 +1,78 @@
-﻿function allowDrop(ev) {
-        ev.preventDefault();
-  }
-  
+﻿
+function allowDrop(ev) {
+    ev.preventDefault();
+    
+}
+
 function drag(ev) {
-        ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
+    ev.dataTransfer.setData("text", ev.target.id);
+    
+}
+
 function drop(ev) {
-        ev.preventDefault();
+    ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
 
-    var whiteFig = ['wP1', 'wP2', 'wP3', 'wP3', 'wP4', 'wP4', 'wP5', 'wP6', 'wP7', 'wP8', 'wR', 'wN', 'wB', 'wQ', 'wK', 'wB2', 'wN2', 'wR2'];
-    var blackFig = ['wP1', 'wP2', 'wP3', 'wP3', 'wP4', 'wP4', 'wP5', 'wP6', 'wP7', 'wP8', 'wR', 'wN', 'wB', 'wQ', 'wK', 'wB2', 'wN2', 'wR2'];
+    // data - current figure id, ev.target - figure under attack
 
-    
-    if (document.getElementById(ev.target.id) != null) {
-        if ((ev.target.id.charAt(0) == 'b' && data.charAt(0) == 'w') || (ev.target.id.charAt(0) == 'w' && data.charAt(0) == 'b')) {
-            // attack
-            ev.target.parentNode.replaceChild(document.getElementById(data), document.getElementById(ev.target.id));         
-        }     
+    if (document.getElementById(ev.target.id) != null) { // if it's attack
+        if ((ev.target.id.charAt(0) == 'b' && data.charAt(0) == 'w') || (ev.target.id.charAt(0) == 'w' && data.charAt(0) == 'b')) { // only different sides can beat each other
+            // attacking   
+            attack(ev);    
+            console.log(data + " has attacked: " + ev.target.id);
+        }
     }
-    else { // simple move  
+    else {
+        // just step 
+        makeStep(ev);
+        console.log(data + " has moved: " + document.getElementById(ev.target.parentNode.id).id);
+    }    
+}
 
+
+function makeStep(ev) {
+    var data = ev.dataTransfer.getData("text");
+    var targetId = ev.target.parentNode.id;
+    var currentId = document.getElementById(data).parentNode.parentNode.id;
+ 
+    if (data.charAt(1) == 'P') {
+        pawnStep(ev, targetId, currentId);
+    }
+    else {
         ev.target.appendChild(document.getElementById(data));
     }
+}
 
-   
-   
-    
-  }
-  
+function attack(ev) {
+    var data = ev.dataTransfer.getData("text");
+    ev.target.parentNode.replaceChild(document.getElementById(data), document.getElementById(ev.target.id));
+}
+
+
+// -- Specific steps --
+
+function pawnStep(ev, targetId, currentId) {
+    var data = ev.dataTransfer.getData("text");
+
+    if (targetId.charAt(0) == currentId.charAt(0)) { // if targer in the same vertical coordinate
+        if (data.charAt(0) == 'w') { // for white
+             // if it's first pawn step
+            if (currentId.charAt(1) == '2' && Number(targetId.charAt(1)) > Number(currentId.charAt(1)) && Number(targetId.charAt(1)) <= Number(currentId.charAt(1)) + 2) {
+                ev.target.appendChild(document.getElementById(data));
+            }
+            else if (Number(targetId.charAt(1)) == Number(currentId.charAt(1)) + 1) {
+                ev.target.appendChild(document.getElementById(data));
+            }            
+        }
+        else if (data.charAt(0) == 'b') { // for black
+            // if it's first pawn step
+            if (currentId.charAt(1) == '7' && Number(targetId.charAt(1)) < Number(currentId.charAt(1)) && Number(targetId.charAt(1)) >= Number(currentId.charAt(1)) - 2) {
+                ev.target.appendChild(document.getElementById(data));
+            }
+            else if (Number(targetId.charAt(1)) == Number(currentId.charAt(1)) - 1) {
+                ev.target.appendChild(document.getElementById(data));
+            }                  
+        }
+    }   
+}
+
